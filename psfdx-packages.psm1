c873cd4379b8c1c92bfef2f9ce9b1bdf1d94ee7d
tonygward <tonygward@googlemail.com>
2021-05-15 10:05:00 +0100

    New and Remove Package

--- psfdx-packages.psm1
+++ psfdx-packages.psm1
@@ -42,5 +42,52 @@ function Get-SalesforcePackage {
     return $packages | Where-Object Name -eq $Name    
 }
 
+function New-SalesforcePackage {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)][string] $Name,
+        [Parameter(Mandatory = $true)][string] $DevHubUsername,
+        [Parameter(Mandatory = $false)][string][ValidateSet("Managed", "Unlocked")] $PackageType,
+        [Parameter(Mandatory = $false)][switch] $IsOrgDependent,
+        [Parameter(Mandatory = $false)][string] $Path = "force-app/main/default",
+        [Parameter(Mandatory = $false)][string] $Description,
+        [Parameter(Mandatory = $false)][switch] $NoNamespace
+    )   
+    if (! $Description) {
+        $Description = $Name
+    }
+    $command = "sfdx force:package:create --name $Name --description $Description" 
+    $command += " --path $Path"  
+    $command += " --packagetype $PackageType"
+    if ($IsOrgDependent) {
+        $command += " --orgdependent"
+    }
+    if ($NoNamespace) {
+        $command += " --nonamespace"
+    }
+    $command += " --targetdevhubusername $DevHubUsername"
+    $command += " --json"
+    $result = Invoke-Sfdx -Command $command
+    $resultSfdx = Show-SfdxResult -Result $result
+    return $resultSfdx.Id
+}
+
+function Remove-SalesforcePackage {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)][string] $Name,
+        [Parameter(Mandatory = $true)][string] $DevHubUsername,
+        [Parameter(Mandatory = $false)][switch] $NoPrompt
+    ) 
+    $command = "sfdx force:package:delete --package $Name"
+    if ($NoPrompt) {
+        $command += " --noprompt"
+    }
+    $command += " --targetdevhubusername $DevHubUsername"
+    Invoke-Sfdx -Command $command
+}  
+
 Export-ModuleMember Get-SalesforcePackages
 Export-ModuleMember Get-SalesforcePackage
+Export-ModuleMember New-SalesforcePackage
+Export-ModuleMember Remove-SalesforcePackage
